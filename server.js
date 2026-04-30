f9a286ba290383896577d9e9a1339c838c568ae2
Ema <emanueleliasrios@gmail.com>
2026-04-29 19:21:36 -0500

    add ejs file

--- server.js
+++ server.js
@@ -11,13 +11,28 @@ export const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.static(join(__dirname, 'public')))
 
 // // Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.render("index", { message: "Hello, World!" });
 });
+app.get("/menu", (req, res) => {
+  res.render("menu");
+});
+app.get("/medicamentos", (req, res) => {
+  res.render("tabla-medicamentos");
+});
+app.get("/registros", (req, res) => {
+  res.render("tabla-registros");
+});
+app.get("/usuarios", (req, res) => {
+  res.render("tabla-usuarios");
+});
+
 // app.use(express.static(__dirname));
 
 // Rutas
